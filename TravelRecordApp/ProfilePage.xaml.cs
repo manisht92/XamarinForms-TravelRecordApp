975b58f1018e82feb5d8a6f8d1470eb068c96cdf
Manish Tiwari <manish.t92@gmail.com>
2018-11-11 23:08:54 +0530

    Linq - Profile Page

--- TravelRecordApp/ProfilePage.xaml.cs
+++ TravelRecordApp/ProfilePage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using SQLite;
+using TravelRecordApp.Model;
 using Xamarin.Forms;
+using System.Linq;
 
 namespace TravelRecordApp
 {
@@ -10,6 +12,31 @@ namespace TravelRecordApp
         public ProfilePage()
         {
             InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                var postTable = conn.Table<Post>().ToList();
+                var categories = (from p in postTable
+                                  orderby p.CategoryId
+                                  select p.CategoryName).Distinct().ToList();
+
+                Dictionary<string, int> categoriesCount = new Dictionary<string, int>();
+
+                foreach (var category in categories)
+                {
+                    var count = (from post in postTable
+                                 where post.CategoryName == category
+                                 select post).ToList().Count;
+                    categoriesCount.Add(category, count);
+                }
+
+                categoriesListView.ItemsSource = categoriesCount;
+
+                postCountLabel.Text = postTable.Count.ToString();
+            }
         }
     }
 }
